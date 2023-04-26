03200c311d5df2dacd5432892592186226969337
fili5863​ <fili5863@stud.kea.dk>
2023-04-26 14:06:39 +0200

    add buyer til array in progress

--- src/components/Header/Buyer.js
+++ src/components/Header/Buyer.js
@@ -1,10 +1,25 @@
 export default function Buyer(props) {
+  const buyer = { ...props, checked: false };
+  console.log("before:", buyer);
+  const potentialBuyersList = [];
+
   return (
     <article key={props.id}>
       <p>Max price: {props.maxPrice}</p>
       <p>Buyer min size: {props.minSize}</p>
       <p>Buyer description: {props.description}</p>
-      <button>Add buyer</button>
+      <button onClick={clickedBuyer}>Add buyer</button>
     </article>
   );
+
+  function clickedBuyer() {
+    return (
+      (buyer.checked = !buyer.checked),
+      console.log("after:", buyer),
+      buyer.checked != false
+        ? potentialBuyersList.push(buyer)
+        : potentialBuyersList.pop(buyer),
+      console.log(potentialBuyersList)
+    );
+  }
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,5 @@
 import "@/styles/globals.css";
 import { Header } from "@/components/Header/Header";
-
 export default function App({ Component, pageProps }) {
   return (
     <>
