6dd2c635ba6f0934d681b817b72946a0669f6c08
Jachob Westh <jachobwesth@gmail.com>
2023-04-27 11:17:53 +0200

    glemte at tilføje buyer heh.. :i

--- src/components/Header/Buyer.js
+++ src/components/Header/Buyer.js
@@ -1,5 +1,6 @@
 import { DistpatchContext } from "@/contexts/buyerContext";
 import { useContext } from "react";
+import { estateTypes } from "@/data/estateTypes";
 
 export default function Buyer(props) {
   const dispatch = useContext(DistpatchContext);
@@ -17,10 +18,18 @@ export default function Buyer(props) {
 
   return (
     <article key={props.id}>
-      <p>{props.id}</p>
+      <p>Buyer ID: #{props.id}</p>
       <p>Max price: {props.maxPrice}</p>
       <p>Buyer min size: {props.minSize}</p>
       <p>Buyer description: {props.description}</p>
+      <p>Takeover date: {props.takeoverDate}</p>
+      <p value={props.estateTypes.id}>
+        Takeover date: {props.estateTypes.name}
+      </p>
+
+      <p>
+        Household: {props.adults}/{props.children}
+      </p>
       <button onClick={addToList}>Add buyer</button>
     </article>
   );
