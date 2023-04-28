df3a3adb3272611a6ad3673655db1faac9052596
Jachob Westh <jachobwesth@gmail.com>
2023-04-28 11:30:46 +0200

    query og state kombineret fra første side

--- src/pages/buyers/index.js
+++ src/pages/buyers/index.js
@@ -31,7 +31,7 @@ export default function Buyers({ data }) {
         <div className={styles.content}>
           <h2>Query params:</h2>
           <pre>
-            <code>{JSON.stringify(state, null, 2)}</code>
+            <code>{JSON.stringify({ ...state, ...query }, null, 2)}</code>
           </pre>
 
           <section className="buyerContainer">
