5f44120cc521287ac7c750cc7686e9735fb7e027
TheFred94 <frederikrlarsen@gmail.com>
2023-04-29 10:54:19 +0200

    form data from the last page is now submitted to Supabase when submitting the form from the last page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -34,25 +34,37 @@ export function ContactForm(query) {
 
   function handleSubmit(e) {
     e.preventDefault();
+    // Stores the form data in a variable
+    const formData = {
+      name: e.target.elements.name.value,
+      email: e.target.elements.email.value,
+      phone: e.target.elements.phone.value,
+      checkbox: e.target.elements.checkbox.checked,
+    };
+
+    // Dispatches the action with the formData as the payload. This is to make sure that it happens before the data is submitted to Supabase, so the newly entered formData is send aswel.
     dispatch({
       action: "MERGE_CONTACT_INFO",
-      payload: {
-        name: e.target.elements.name.value,
-        email: e.target.elements.email.value,
-        phone: e.target.elements.phone.value,
-        checkbox: e.target.elements.checkbox.checked,
-      },
+      payload: formData,
     });
+    // Spreads out the ...query and the ...formData and puts the data in the right columns in Supabase. Column naming is not perfect in Supabase using this method.
     const payload = {
-      name: query.name,
-      email: query.email,
-      phone: query.phone,
-      price: query.price,
-      size: query.size,
-      zip: query.zipCode,
-      consent: query.checkbox,
+      ...query,
+      ...formData,
       buyersList: query.buyersList,
+
+      // name: query.name,
+      // email: query.email,
+      // phone: query.phone,
+      // price: query.price,
+      // size: query.minSize,
+      // zip: query.zipCode,
+      // propertyType: query.propertyType,
+      // consent: query.checkbox,
+      // buyersList: query.buyersList,
     };
+
+    console.log(payload);
     fetch("/api/addData", {
       method: "POST",
       headers: {
