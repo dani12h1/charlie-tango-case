f94fc69e27243d52c71a40897cb53619128f9a05
TheFred94 <frederikrlarsen@gmail.com>
2023-05-01 15:17:27 +0200

    getting data into the dashboard works

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
+import styles from "./Home.module.css";
+import Buyer from "@/components/Header/Buyer";
 
-export default function Dashboard() {
+export default function Dashboard({ data }) {
+  const [dashboardBuyers, setDashboardBuyers] = useState([]);
   const [page, setPage] = useState(0);
 
   useEffect(() => {
@@ -11,8 +14,44 @@ export default function Dashboard() {
       },
     })
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        console.log(data), setDashboardBuyers(data.response);
+      });
   }, [page]);
 
-  return <div>dashboard</div>;
+  console.log(dashboardBuyers);
+
+  return (
+    <>
+      <div className="wrapper">
+        <h1 className={styles.headline}>Potential buyers</h1>
+        <section className="buyerContainer">
+          <DashboardList dashboardBuyers={dashboardBuyers} />
+        </section>
+      </div>
+    </>
+  );
+}
+
+function DashboardList(props) {
+  return (
+    <ul>
+      <h2>List of products</h2>
+      {/* Receives the props.artcles from the App component */}
+      {props.dashboardBuyers.map((buyerSeller) => (
+        // Sends down the props.buyProduct received from the App
+        <BuyerSeller key={props.id} buyerSeller={{ ...buyerSeller }} />
+      ))}
+    </ul>
+  );
+}
+
+function BuyerSeller(props) {
+  return (
+    <section>
+      <p>Name: {props.buyerSeller.name}</p>
+      <p>Email: {props.buyerSeller.email}</p>
+      <p>Phone: {props.buyerSeller.phone}</p>
+    </section>
+  );
 }
