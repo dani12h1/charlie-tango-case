ec12adc46e631eedaaebbdbccb3a9fb8896e3d7a
TheFred94 <frederikrlarsen@gmail.com>
2023-05-01 15:24:44 +0200

    formatted date format

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -47,11 +47,17 @@ function DashboardList(props) {
 }
 
 function BuyerSeller(props) {
+  const createdAt = new Date(props.buyerSeller.created_at);
+  const formattedDate = `${createdAt.getDate()}-${
+    createdAt.getMonth() + 1
+  }-${createdAt.getFullYear()} ${createdAt.getHours()}:${createdAt.getMinutes()}`;
+
   return (
     <section>
       <p>Name: {props.buyerSeller.name}</p>
       <p>Email: {props.buyerSeller.email}</p>
       <p>Phone: {props.buyerSeller.phone}</p>
+      <p>Created at: {formattedDate}</p>
     </section>
   );
 }
