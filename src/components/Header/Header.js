8b22064874728ab0278676d7929eeb1643150a36
TheFred94 <frederikrlarsen@gmail.com>
2023-05-02 15:13:08 +0200

    showing the length of the DashboardBuyers in the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,15 +4,9 @@ import styles from "./Header.module.css";
 import Link from "next/link";
 import { useContext } from "react";
 import { DashboardContext } from "@/pages/_app";
-import { useEffect } from "react";
 
 export function Header() {
   const dashboardBuyers = useContext(DashboardContext);
-
-  // Only to show the length of the array
-  // useEffect(() => {
-  //   console.log(dashboardBuyers.length);
-  // });
 
   return (
     <>
@@ -43,7 +37,13 @@ export function Header() {
                 d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
               />
             </svg>
-            <div>{dashboardBuyers.length}</div>
+            <div>
+              {" "}
+              {
+                dashboardBuyers.filter((seller) => seller.contacted === false)
+                  .length
+              }
+            </div>
           </button>
         </Link>
       </header>
