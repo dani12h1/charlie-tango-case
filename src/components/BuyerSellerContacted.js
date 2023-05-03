5fbb7b79cf4f6703fd0d091757cfa2ade6052976
Jachob Westh <jachobwesth@gmail.com>
2023-05-03 11:11:50 +0200

    init commit

--- src/components/BuyerSellerContacted.js
+++ src/components/BuyerSellerContacted.js
@@ -1,11 +1,11 @@
 // One single buyer and seller on the ContactedList
 export function BuyerSellerContacted(props) {
   return (
-    <section>
+    <article>
       <p>Name: {props.contacted.name}</p>
       <p>Email: {props.contacted.email}</p>
       <p>Phone: {props.contacted.phone}</p>
       <p>Contacted: {`${props.contacted.contacted}`}</p>
-    </section>
+    </article>
   );
 }
--- src/components/ContactedList.js
+++ src/components/ContactedList.js
@@ -5,14 +5,14 @@ export function ContactedList(props) {
   return (
     <>
       <h2>Closed tickets</h2>
-      <ul className="ContactedList">
+      <div className="ContactedList">
         {props.dashboardBuyers.map((contacted) => (
           <BuyerSellerContacted
             key={contacted.id}
             contacted={{ ...contacted }}
           />
         ))}
-      </ul>
+      </div>
     </>
   );
 }
